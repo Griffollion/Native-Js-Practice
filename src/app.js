995e97ac6c4656eb011f2254c48235964dad424d
Griffollion <philippovichdenis@gmail.com>
2020-02-25 17:54:40 +0300

    add form and input validation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,29 @@
 
+import {isValid} from  './utils'
 import './styles.css'
 
-console.log('App is running');
+
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+form.addEventListener('submit', submitFormhandler)
+
+function submitFormhandler(event) {
+    event.preventDefault()
+
+    if(isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+
+        submitBtn.disabled = true
+        // Async request to server to save question
+        console.log('Question:', question)
+
+        input.value = ''
+        input.className = ''
+        submitBtn.disabled = false
+    }
+}
